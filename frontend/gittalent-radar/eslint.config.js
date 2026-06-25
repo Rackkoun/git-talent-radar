ebb8b947d90d604bd7bd2508a115dfd7f25f93d4
Rackkoun <ruphustchana@yahoo.com>
2026-06-25 22:17:53 +0200

    test: test, linting and ci bug solved

--- frontend/gittalent-radar/eslint.config.js
+++ frontend/gittalent-radar/eslint.config.js
@@ -5,6 +5,13 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 
 export default tseslint.config(
+  {
+    ignores: [
+      "dist/**",
+      "coverage/**",
+      "node_modules/**",
+    ],
+  },
   js.configs.recommended,
 
   ...tseslint.configs.recommended,
--- frontend/gittalent-radar/vitest.config.ts
+++ frontend/gittalent-radar/vitest.config.ts
@@ -12,7 +12,9 @@ export default defineConfig({
             provider: "v8",
             reporter: ["text", "json", "html"],
             exclude: [
-                "node_modules/",
+                "dist/**",
+                "coverage/**",
+                "node_modules/**",
                 "src/test/",
                 "**/*.d.ts",
                 "**/*.config.*",
